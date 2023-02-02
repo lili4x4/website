fba04bcf95ec63fe7aa92f7f8b07d0d9595cafff
Liliana <liliana.m.parra@gmail.com>
2023-02-02 18:26:05 -0500

    Change resume to link to Google Drive

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import { Header } from '../components/Header';
-import myResume from '../assets/Resume-Liliana-Parra.pdf'
 
 function Resume() {
 return (
     <div className="App">
         <Header pageTitle="Resume"/>
-        <a href={myResume} target="_blank" rel="noreferrer">
+        <a href="https://drive.google.com/file/d/1FVMmqY3K8V2KqcPE4e30t5uIpv5UXQUr/view?usp=share_link" target="_blank" rel="noreferrer">
             <h3>Open in separate tab</h3>
         </a>
-        <iframe 
-            title="Liliana Parra Resume PDF" 
-            src={require("../assets/Resume-Liliana-Parra.pdf")}
-            width="100%"
-            height="500px"
-        />
     </div>
 );
 }
