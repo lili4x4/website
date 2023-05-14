8f8e2fcf7fb360bbd597237241da54437452a1c3
Liliana <liliana.m.parra@gmail.com>
2023-05-14 17:48:31 -0400

    Added controls to mobile and fixed control height issue

--- src/components/HomeDesktop.tsx
+++ src/components/HomeDesktop.tsx
@@ -7,27 +7,27 @@ interface Props {
 export const HomeDesktop: React.FC <Props>= (props) => {
     return (
         <>
-        <div id="home" className="carousel slide" data-ride="carousel">
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <img className="d-block w-100" src={require('../assets/ILEA1.jpg')} alt="First slide" />
+            <div id="home" className="carousel slide" data-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img className="d-block w-100" src={require('../assets/ILEA1.jpg')} alt="First slide" />
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src={require('../assets/HauntedMuseum1.jpg')} alt="Second slide" />
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src={require('../assets/ILEA2.jpg')} alt="Third slide" />
+                    </div>
                 </div>
-                <div className="carousel-item">
-                    <img className="d-block w-100" src={require('../assets/HauntedMuseum1.jpg')} alt="Second slide" />
-                </div>
-                <div className="carousel-item">
-                    <img className="d-block w-100" src={require('../assets/ILEA2.jpg')} alt="Third slide" />
-                </div>
-             </div>
-        </div>
-        <a className="carousel-control-prev" href="#home" role="button" data-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="sr-only">Previous</span>
-  </a>
-  <a className="carousel-control-next" href="#home" role="button" data-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="sr-only">Next</span>
-  </a>
+            </div>
+            <a className="carousel-control-prev" href="#home" role="button" data-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="sr-only">Previous</span>
+            </a>
+            <a className="carousel-control-next" href="#home" role="button" data-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="sr-only">Next</span>
+            </a>
         </>
         
     )
--- src/components/HomeMobile.tsx
+++ src/components/HomeMobile.tsx
@@ -6,18 +6,28 @@ interface Props {
 
 export const HomeMobile: React.FC <Props>= (props) => {
     return (
-        <div id="home" className="carousel slide" data-ride="carousel">
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <img className="d-block w-100" src={require('../assets/MobileHome1.jpg')} alt="First slide" />
+        <>
+            <div id="home" className="carousel slide" data-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img className="d-block w-100" src={require('../assets/MobileHome1.jpg')} alt="First slide" />
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src={require('../assets/MobileHome2.jpg')} alt="Second slide" />
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src={require('../assets/MobileHome3.jpg')} alt="Third slide" />
+                    </div>
                 </div>
-                <div className="carousel-item">
-                    <img className="d-block w-100" src={require('../assets/MobileHome2.jpg')} alt="Second slide" />
-                </div>
-                <div className="carousel-item">
-                    <img className="d-block w-100" src={require('../assets/MobileHome3.jpg')} alt="Third slide" />
-                </div>
-             </div>
-        </div>
+            </div>
+            <a className="carousel-control-prev" href="#home" role="button" data-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="sr-only">Previous</span>
+            </a>
+            <a className="carousel-control-next" href="#home" role="button" data-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="sr-only">Next</span>
+            </a>
+        </>
     )
 }
