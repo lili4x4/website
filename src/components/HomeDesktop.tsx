de3f4c9270cf3e2b0bfc271e6410be3a8674362f
Liliana <liliana.m.parra@gmail.com>
2023-05-14 17:33:13 -0400

    Added controls to carousel

--- src/components/HomeDesktop.tsx
+++ src/components/HomeDesktop.tsx
@@ -6,6 +6,7 @@ interface Props {
 
 export const HomeDesktop: React.FC <Props>= (props) => {
     return (
+        <>
         <div id="home" className="carousel slide" data-ride="carousel">
             <div className="carousel-inner">
                 <div className="carousel-item active">
@@ -19,5 +20,15 @@ export const HomeDesktop: React.FC <Props>= (props) => {
                 </div>
              </div>
         </div>
+        <a className="carousel-control-prev" href="#home" role="button" data-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="sr-only">Previous</span>
+  </a>
+  <a className="carousel-control-next" href="#home" role="button" data-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="sr-only">Next</span>
+  </a>
+        </>
+        
     )
 }
