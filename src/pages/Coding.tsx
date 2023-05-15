176ca1110479a8ea1bb07ad61f50e1f78dbf5dc3
Liliana <liliana.m.parra@gmail.com>
2023-05-15 14:13:24 -0400

    Added background color to Coding page

--- src/pages/Coding.tsx
+++ src/pages/Coding.tsx
@@ -21,17 +21,19 @@ function Coding() {
     });
     
     return (
-        <div className="main-content padding-bottom">
-            <Header pageTitle="Lili Codes"/>
-            <p>
-                Once upon a time, during a pandemic, Lili re-evaluated her life and decided to finally try out coding.
-                She went all in, even applying and being admitted to Ada Developers Academy, a full-stack web development bootcamp.
-                She learned a LOT, mostly that she can do anything she sets her mind to. After doing a couple of interships in software development, though,
-                she also learned that she likes coding more as a hobby than as a career. 
-            </p>
-            <p className='margin-bottom'>Check out some of Lili's coding projects below!</p>
-            <Header pageTitle="Coding Portfolio"/>
-            <section>{PortfolioEntries}</section>
+        <div className="App">
+            <div className="main-content padding-bottom">
+                <Header pageTitle="Lili Codes"/>
+                <p>
+                    Once upon a time, during a pandemic, Lili re-evaluated her life and decided to finally try out coding.
+                    She went all in, even applying and being admitted to Ada Developers Academy, a full-stack web development bootcamp.
+                    She learned a LOT, mostly that she can do anything she sets her mind to. After doing a couple of interships in software development, though,
+                    she also learned that she likes coding more as a hobby than as a career. 
+                </p>
+                <p className='margin-bottom'>Check out some of Lili's coding projects below!</p>
+                <Header pageTitle="Coding Portfolio"/>
+                <section>{PortfolioEntries}</section>
+            </div>
         </div>
     );
 }
